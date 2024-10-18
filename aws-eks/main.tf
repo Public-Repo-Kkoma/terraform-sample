98bd84a8c163f319e73f35a686788551d3cb5175
taehwankim <wih0202@gmail.com>
2024-10-19 00:04:34 +0900

    code refactoring

--- aws-eks/main.tf
+++ aws-eks/main.tf
@@ -2,28 +2,28 @@ terraform {
   required_version = ">= 1.0"
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.70.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.70.0"
     }
     kubernetes = {
-        source = "hashicorp/kubernetes"
-        version = ">= 2.33.0"
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.33.0"
     }
   }
 }
 
 provider "aws" {
-  
+
 }
-module "demo_vpc" {
-  source = "./modules/vpc"
-  vpc_name = "dev"
+module "sample_vpc" {
+  source   = "./vpc"
+  vpc_name = "dev-vpc"
   vpc_cidr = "10.0.0.0/16"
 }
-module "demo_eks" {
-  source = "./modules/eks"
-  cluster_name = "dev-cluster"
+module "sample_eks" {
+  source          = "./eks"
+  cluster_name    = "dev-cluster"
   cluster_version = "1.30"
-  vpc_id = module.demo_vpc.vpc_id
-  private_subnets = module.demo_vpc.private_subnets
+  vpc_id          = module.sample_vpc.vpc_id
+  private_subnets = module.sample_vpc.private_subnets
 }
