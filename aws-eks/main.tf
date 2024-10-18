e3b0de35d0cbd38baf068f54e38d4ad41faaa368
thkim <th313@wavve.com>
2024-10-18 10:01:16 +0900

    aws-eks-code refactoring

--- aws-eks/main.tf
+++ aws-eks/main.tf
@@ -16,12 +16,12 @@ provider "aws" {
   
 }
 module "demo_vpc" {
-  source = "../modules/vpc"
+  source = "./modules/vpc"
   vpc_name = "dev"
   vpc_cidr = "10.0.0.0/16"
 }
 module "demo_eks" {
-  source = "../modules/eks"
+  source = "./modules/eks"
   cluster_name = "dev-cluster"
   cluster_version = "1.30"
   vpc_id = module.demo_vpc.vpc_id
